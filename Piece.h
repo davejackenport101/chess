497c34d8e73be10cfe0fe59bb69e2088e2d303d2
Jack Davenport <jdavenport1210@gmail.com>
2022-01-24 19:41:18 -0600

    file renaming/organizing

---
+++ Piece.h
@@ -0,0 +1,36 @@
+/**
+ * @file piece.h
+ * @author your name (you@domain.com)
+ * @brief Header file for an abstract piece object
+ * @version 0.1
+ * @date 2022-01-24
+ * 
+ * @copyright Copyright (c) 2022
+ */
+
+#pragma once
+
+using namespace std;
+class Piece {
+    public:
+        //returns coordinates for where the piece moved
+        virtual pair move(int dir);
+
+
+
+    private:
+        /**
+         * If this piece is still on the board, this value holds which one this piece is
+         * If the piece is not on the board this value becomes -1
+         * 0: pawn
+         * 1: bishop
+         * 2: knight
+         * 3: rook
+         * 4: Queen
+         * 5: King
+         */
+        int value;
+
+
+
+};
---
+++ main.cpp
@@ -0,0 +1,13 @@
+/**
+ * @file main.cpp
+ * @author Jack Davenport (you@domain.com)
+ * @brief At home attempt at an Implementation of Chess 
+ * @version 0.1
+ * @date 2022-01-24
+ * 
+ * @copyright Copyright (c) 2022 lmao this line makes it seem super official
+ */
+
+void main() {
+    return;
+}
